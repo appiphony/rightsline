e57f8472fd614d126fc93b1c959147166cf4913f
Katie Conway <katieconway@gmail.com>
2022-07-11 10:59:51 -0500

    field mapping

---
+++ force-app/main/default/lwc/dataMapper/dataMapper.js
@@ -0,0 +1,321 @@
+import { LightningElement, api, track } from "lwc";
+import saveData from '@salesforce/apex/setupAssistant.saveData';
+
+export default class DataMapper extends LightningElement {
+    showRemove = false;
+    @api step
+    @api defaultMapping = {
+        rows: [
+            {
+                sfOptions: [],
+                tpOptions: []
+            }
+        ]
+    }
+    @api sfColumnName
+    @api tpColumnName
+    @api hasNoUnique
+    @track mapping
+    @track sfOptions
+    @track tpOptions
+
+    @api 
+    get sfObject() {
+        return this.mapping ? this.mapping.sfObject : null
+    }
+
+    set sfObject(value) {
+        this._sfObject = value
+        if(this.sfOptions && this.tpOptions) this.setOptions()
+    }
+
+    @api 
+    get tpFields() {
+        return this.tpFields
+    }
+
+    set tpFields(fields) {
+        if(fields) {
+            if(fields.length && !fields[0].label) this.hasStaticTp = true
+            this.tpOptions = fields
+            if(this.sfOptions) this.setOptions()
+        }
+    }
+
+    @api
+    get sfFields() {
+        return this.sfFields
+    }
+
+    set sfFields(fields) {
+        if(fields) {
+            if(fields.length && !fields[0].label) this.hasStaticSf = true
+            this.sfOptions = fields
+            if(this.tpOptions) this.setOptions()
+        }
+    }
+
+    @api
+    get setupData() {
+        return this.setupData
+    }
+
+    set setupData(setupData) {
+        if(setupData) {
+            this.mappings = setupData.Data_Mappings__c ? JSON.parse(setupData.Data_Mappings__c) : {}
+            if(this.mappings[this.step]) {
+                let mapping = JSON.parse(JSON.stringify(this.mappings[this.step]))
+                mapping.rows.forEach(row => {
+                    row.id = Math.random()
+                    row.sfOptions = this.sfOptions
+                    row.tpOptions = this.tpOptions
+                })
+                this.mapping = mapping
+            }
+            if(this.tpOptions || this.sfOptions) this.setOptions()
+        }
+    }
+
+    @api
+    get showHeader() {
+        return this._showHeader !== false;
+    }
+    set showHeader(showHeader) {
+        this._showHeader = showHeader && showHeader !== "false";
+    }
+
+    get header() {
+        return this.showHeader ? "" : "slds-hide";
+    }
+
+    // CB EDIT
+    get headerStyle() {
+        return "width: " + 100.0 / 2 + "%;";
+    }
+
+    get colSpan() {
+        return this.showRemove ? 3 : 2;
+    }
+
+    get addButton() {
+        return this.mapping && !this.hasStaticSf && !this.hasStaticTp ? this.showRemove && this.mapping.rows.length < this.maxMappings : false
+    }
+
+    get hasMappings() {
+        return this.mapping ? this.mapping.rows.length : false
+    }
+
+    // CB EDIT
+    get showRemove() {
+        return this._sfObject
+    }
+
+    @api
+    handleObjectChange(fields) {
+        delete this.mappings[this.step]
+        this.saveMappings()
+    }
+
+    @api
+    validate() {
+        return new Promise((resolve, reject) => {
+            if(!this.getRequiredRows().length && (this.hasStaticSf || this.hasStaticTp) || this.getCompleteRows().length) {
+                if(!this.hasStaticSf && !this.hasStaticTp && !this.mappings[this.step]) {
+                    this.mappings[this.step] = JSON.parse(JSON.stringify(this.mapping))
+                    this.removeIncompleteRows()
+                    this.saveMappings().then(() => {
+                        resolve()
+                    }).catch(error => {
+                        reject(error)
+                    })
+                } else {
+                    this.removeIncompleteRows()
+                    resolve()
+                }
+            } else {
+                reject('No rows Mapped or Missing Required Fields')
+            }
+        })
+    }
+
+    setDefaultMapping() {
+        this.mapping = JSON.parse(JSON.stringify(this.defaultMapping))
+        let required = this.getRequiredRows()
+
+        if(required.length) this.mapping = JSON.stringify(this.defaultMapping) !== JSON.stringify({rows: [{sfOptions:[], tpOptions:[]}]}) ? this.mapping : {rows: []}
+        let allRequired = [...this.tpOptions, ...this.sfOptions].filter(option => option.required === true)
+    
+        this.mapping.rows.forEach(row => {
+            row.id = Math.random()
+            row.sfOptions = this.sfOptions
+            row.tpOptions = this.tpOptions
+            row.deletable = this.hasStaticSf || this.hasStaticTp || allRequired.filter(requiredField => requiredField.value === row.tpField || requiredField.value === row.sfField).length
+            row.sfReq = allRequired.filter(requiredField => requiredField.value === row.sfField).length
+            row.tpReq = allRequired.filter(requiredField => requiredField.value === row.tpField).length
+        })
+        if(required.length) {
+            required.forEach(option => {
+                this.mapping.rows.push({
+                    id: Math.random(),
+                    sfField: option.column === 'sf' ? option.value : null,
+                    tpField: option.column === 'tp' ? option.value : null,
+                    sfReq: option.column === 'sf',
+                    tpReq: option.column === 'tp',
+                    fieldType: option.fieldType,
+                    sfOptions: this.sfOptions,
+                    tpOptions: this.tpOptions,
+                    deletable: true
+                })
+            })
+        }
+        this.updateOptions()
+    }
+
+    setOptions() {
+        if(this.sfOptions.length && this.tpOptions.length) {
+            this.maxMappings = this.sfOptions.length < this.tpOptions.length ? this.sfOptions.length : this.tpOptions.length
+        } else if(this.sfOptions.length) {
+            this.maxMappings = this.sfOptions.length
+        } else if(this.tpOptions.length) {
+            this.maxMappings = this.tpOptions.length
+        }
+
+        if(this.mappings && !this.mappings[this.step] && this._sfObject) {
+            this.setDefaultMapping()
+        } else if(this.mapping) {
+            this.mapping.rows.forEach(row => {
+                row.sfOptions = this.sfOptions
+                row.tpOptions = this.tpOptions
+            })
+            this.updateOptions()
+        }
+    }
+
+    updateOptions(event) {
+        let selected = []
+        let rowIndex = event ? parseInt(event.currentTarget.dataset.rowindex) : null
+        let fieldType = event ? event.currentTarget.dataset.fieldType : null
+
+        this.mapping.rows.forEach(row => {
+            if(row.sfField) selected.push(row.sfField)
+            if(row.tpField) selected.push(row.tpField)
+        })
+
+        if(!this.hasNoUnique) {
+            this.sfOptions.forEach(option => {
+                if(option.label) option.disabled = selected.includes(option.value)
+            })
+
+            this.tpOptions.forEach(option => {
+                if(option.label) option.disabled = selected.includes(option.value)
+            })
+        }
+
+        if(fieldType) {
+            this.mapping.rows[rowIndex].sfOptions = !this.hasStaticSf ? this.sfOptions.filter(option => option.fieldType === fieldType) : this.sfOptions
+            this.mapping.rows[rowIndex].tpOptions = !this.hasStaticTp ? this.tpOptions.filter(option => option.fieldType === fieldType) : this.tpOptions
+        } else if(rowIndex !== null) {
+            this.mapping.rows[rowIndex].sfOptions = this.sfOptions
+            this.mapping.rows[rowIndex].tpOptions = this.tpOptions
+        }
+        this.sfOptions = [...this.sfOptions]
+        this.tpOptions = [...this.tpOptions]
+    }
+
+    updateRow(event) {
+        let rowIndex = parseInt(event.currentTarget.dataset.rowindex)
+        let column = event.currentTarget.dataset.column
+        let options = [...this.tpOptions, ...this.sfOptions].filter(option => option.value === event.detail.value || option.value === event.target.value)
+        let fieldType = options.length ? options[0].fieldType : null
+        let currentSfField = this.mapping.rows[rowIndex].sfField
+        let currentTpField = this.mapping.rows[rowIndex].tpField
+        let previousFieldValue = column === 'sf' ? currentSfField : currentTpField
+
+        let sfField = column === 'sf' ? event.detail.value ? event.detail.value : event.target.value : currentSfField
+        let tpField = column === 'tp' ? event.detail.value ? event.detail.value : event.target.value : currentTpField
+
+        this.mapping.rows[rowIndex].sfField = sfField
+        this.mapping.rows[rowIndex].tpField = tpField
+        this.mapping.rows[rowIndex].fieldType = fieldType
+
+        if(!this.getRequiredRows().length && sfField && tpField) {
+            this.mappings[this.step] = JSON.parse(JSON.stringify(this.mapping))
+            this.mappings[this.step].sfObject = this._sfObject
+            this.removeIncompleteRows()
+            this.saveMappings()
+        }
+    }
+
+    removeRow(event) {
+        let rowIndex = parseInt(event.currentTarget.dataset.rowindex)
+        this.mapping.rows.splice(rowIndex, 1)
+        this.mappings[this.step] = JSON.parse(JSON.stringify(this.mapping))
+        this.removeIncompleteRows()
+
+        if(!this.getRequiredRows().length && this.mappings[this.step].rows.length) {
+            this.saveMappings()
+        } else if(!this.mappings[this.step].rows.length && this.mapping.rows.length) {
+            delete this.mappings[this.step]
+            this.saveMappings() 
+        } else if(!this.mapping.rows.length) {
+            delete this.mappings[this.step]
+            this.saveMappings()
+            this.setDefaultMapping()
+        }
+    }
+
+    addRow() {
+        this.mapping.rows.push({
+            id: Math.random(),
+            sfField: null,
+            tpField: null,
+            sfOptions: this.sfOptions,
+            tpOptions: this.tpOptions
+        })
+        this.updateOptions()
+    }
+
+    getRequiredRows() {
+        let required = [...this.tpOptions, ...this.sfOptions].filter(option => option.required === true)
+        return required.filter(requiredField => !this.mapping.rows.filter(mappedRow => (mappedRow.tpField === requiredField.value && mappedRow.sfField) || (mappedRow.sfField === requiredField.value && mappedRow.tpField)).length)
+    }
+
+    saveMappings() {
+        this.cleanMapping()
+        return new Promise((resolve, reject) => {
+            saveData({
+                setupData: {
+                    Data_Mappings__c: JSON.stringify(this.mappings) !== '{}' ? JSON.stringify(this.mappings) : null
+                }
+            }).then(response => {
+                let responseData = JSON.parse(response);
+                if(responseData.isSuccess) {
+                    resolve()
+                } else {
+                    this.showToast(responseData.error, 'error', 'pester')
+                    reject(responseData.error)
+                }
+            })
+        })
+    }
+
+    getCompleteRows() {
+        return this.mapping.rows.filter(row => row.sfField && row.tpField)
+    }
+
+    removeIncompleteRows() {
+        if(this.hasStaticSf || this.hasStaticTp) return
+        this.mappings[this.step].rows = this.mappings[this.step].rows.filter(row => (row.sfField && row.tpField) || row.required)
+    }
+
+    cleanMapping() {
+        if(this.mappings[this.step]) {
+            this.mappings[this.step].rows.forEach(row => {
+                delete row['id']
+                delete row['fieldType']
+                delete row['sfOptions']
+                delete row['tpOptions']
+            })
+        }
+    }
+}
---
+++ force-app/main/default/lwc/fieldMappingStep/fieldMappingStep.js
@@ -0,0 +1,277 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import saveData from '@salesforce/apex/setupAssistant.saveData';
+import getData from '@salesforce/apex/setupAssistant.getData';
+
+export default class FieldMappingStep extends LightningElement {
+
+    @track activeSection = 'group'; //Selected Vertical Nav Tab. Defaults to first available section
+
+    get isGroup() {
+        return this.activeSection === "group";
+    }
+    get isClient() {
+        return this.activeSection === "client";
+    }
+
+    @track mappingName = 'Account - Customer Account'
+
+
+    get objectOptions() {
+        return [
+            {
+                label: 'Account',
+                value: 'account',
+            },
+            {
+                label: 'Contact',
+                value: 'contact',
+            },
+        ]
+    }
+
+    @track objectValue = 'account'
+    @track objectLabel = 'Account'
+
+
+
+    updateObjectValue(event){
+        this.value = event.detail.value;
+        this.value = objectValue;
+    }
+
+    @track recordTypeValue = 'customerAccount'
+    get recordTypeOptions() {
+        return [
+            {
+                label: 'Customer Account',
+                value: 'customerAccount',
+            },
+            {
+                label: 'Sales Account',
+                value: 'salesAccount',
+            },
+        ]
+    }
+
+    @track rlValue = 'customer'
+
+    get rlOptions() {
+        return [
+            {
+                label: 'Customer',
+                value: 'customer',
+            },
+            {
+                label: 'Sales',
+                value: 'sales',
+            },
+        ]
+    }
+    
+    handleSectionSelect(event) {
+        this.activeSection = event.detail.name;
+        this.activeMapping = this[this.activeSection + 'Mapping'];
+    }
+
+    updateValue(event){
+        let value = event.detail.value;
+        this.value = value;
+    } 
+
+
+    @track selectedObject = 'Account'
+    @track setupData
+    @track defaultMapping = {
+        rows: [
+            {
+                tpField: 'salutation',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'phonePrimary',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'email',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'addressLine1',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'hAddressLine1',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'birthYear',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'shippingCarrierName',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'rlContactId',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'firstName',
+                sfField: 'sfield1'
+            },
+            {
+                tpField: 'phoneOther',
+                sfField: 'sfield1'
+            },
+        ]
+    }
+
+    @track tpFields = [
+        {
+            label: 'Salutation',
+            value: 'salutation',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'Phone (Primary)',
+            value: 'phonePrimary',
+            required: true,
+            column: 'tp',
+            fieldType: 'integer'
+        },
+
+        {
+            label: 'Email',
+            value: 'email',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'Address Line 1',
+            value: 'addressLine1',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: '(H) Address Line 1',
+            value: 'hAddressLine1',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'Birth Year',
+            value: 'birthYear',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'Shipping Carrier Name',
+            value: 'shippingCarrierName',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'RL Contact ID',
+            value: 'rlContactId',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'First Name',
+            value: 'firstName',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+        {
+            label: 'Phone (Other)',
+            value: 'phoneOther',
+            required: true,
+            column: 'tp',
+            fieldType: 'string'
+        },
+    ]
+
+    @track sfFields = [
+        {
+            label: 'SF String 1',
+            value: 'sfield1',
+            column: 'sf',
+            fieldType: 'string'
+        },
+        {
+            label: 'SF Integer 1',
+            value: 'sfield3',
+            column: 'sf',
+            fieldType: 'integer'
+        },
+        {
+            label: 'SF Integer 2',
+            value: 'sfield4',
+            column: 'sf',
+            fieldType: 'integer'
+
+        },
+        {
+            label: 'SF Boolean 1',
+            value: 'sfield5',
+            column: 'sf',
+            fieldType: 'boolean'
+        }
+    ]
+
+    // constructor() {
+    //     super()
+    //     this.template.addEventListener('next', this.next.bind(this))
+    // }
+
+    @api
+    show() {
+        return new Promise((resolve, reject) => {
+            getData().then(response => {
+                let responseData = JSON.parse(response)
+                if(responseData.isSuccess) {
+                    this.setupData = responseData.results.setupData
+                    this.loading = false
+                    resolve()
+                } else {
+                    console.log(response)
+                }
+            })
+        })
+    }
+
+    next(event) {
+        event.stopPropagation();
+        this.template.querySelector('c-data-mapper').validate().then(() => {
+            saveData({
+                setupData: {
+                    Steps_Completed__c: JSON.stringify({
+                        'C-DATA-MAPPER-STEP': 1
+                    })
+                }
+            }).then(response => {
+                let responseData = JSON.parse(response);
+                if(responseData.isSuccess) {
+                    this.dispatchEvent(new CustomEvent('next', {
+                        bubbles: true,
+                        composed: true
+                    }));
+                } else {
+                    this.showToast(responseData.error, 'error', 'pester')
+                }
+            })
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+}
