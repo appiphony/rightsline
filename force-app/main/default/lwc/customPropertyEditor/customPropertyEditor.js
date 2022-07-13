bc73fbfafcf0eb555e25fbce9166b592df245fe5
danielferenc <daniel.ferenc@appiphony.com>
2022-07-13 09:30:04 -0500

    Custom Property Editor Mock up

--- force-app/main/default/lwc/customPropertyEditor/customPropertyEditor.js
+++ force-app/main/default/lwc/customPropertyEditor/customPropertyEditor.js
@@ -1,15 +1,50 @@
 import { LightningElement, track, api } from 'lwc';
 // import getTemplates from '@salesforce/apex/makeCallout.getTemplates'
 
-export default class InboundConnectorStep extends LightningElement {
-    @api objectAPI;
-    @api recordID;
+export default class customPropertyEditor extends LightningElement {
     @api operation;
 
     picklistOptions = [
         {
-            label: '1',
-            value: '1'
+            label: 'Create',
+            value: 'Create'
+        },
+        {
+            label: 'Update',
+            value: 'Update'
+        },
+        {
+            label: 'Delete',
+            value: 'Delete'
         }
     ]
+    
+    connectedCallback(){
+        //
+    }
+
+    handleActionSelect(event) {
+        event.currentTarget.value = event.detail.value;
+        let newValue = {
+            operation: event.currentTarget.value,
+            recordId: '{!$Record.Id}'
+        };
+
+        const valueChangedEvent = new CustomEvent(
+            'configuration_editor_input_value_changed',
+            {
+                bubbles: true,
+                cancelable: false,
+                composed: true,
+                detail: {
+                    name: 'data',
+                    newValue: JSON.stringify(newValue),
+                    newValueDataType: 'String'
+                }
+            }
+        );
+        debugger;
+
+        this.dispatchEvent(valueChangedEvent);
+    }
 }
